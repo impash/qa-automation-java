103b337358fa8b2f399b6f24c9bb09c63bea3bb1
p.shtyrkov <impash@mail.ru>
2021-08-11 18:25:19 +0300

    add app as maven-project, up jUnit-version to 5.7.2

--- src/com/tinkoff/edu/MyApplication.java
+++
@@ -1,9 +0,0 @@
-package com.tinkoff.edu;
-
-import com.tinkoff.edu.printer.Printer;
-
-public class MyApplication {
-    public static void main(String[] args) {
-        Printer.print("Hello world!");
-    }
-}
